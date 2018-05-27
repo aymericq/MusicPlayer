bf43bd5006db5ca338f899ed21b31e8c654e2de7
aymericq <aquesne@gmail.com>
2018-05-27 10:51:48 +0200

    First implementation of settings view

--- app/src/main/java/com/centralesupelec/ipfs/ipfsplayer/SettingAdapter.java
+++ app/src/main/java/com/centralesupelec/ipfs/ipfsplayer/SettingAdapter.java
@@ -1,4 +1,60 @@
 package com.centralesupelec.ipfs.ipfsplayer;
 
-public class MyAdapter {
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import com.centralesupelec.ipfs.ipfsplayer.R;
+
+public class SettingAdapter extends RecyclerView.Adapter<SettingAdapter.ViewHolder> {
+    private String[] mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public ConstraintLayout mConstraintLayout;
+        public TextView mSettingNameTextView;
+        public Switch mSwitch;
+        public ViewHolder(ConstraintLayout cl) {
+            super(cl);
+            mConstraintLayout = cl;
+            mSettingNameTextView = (TextView) cl.getChildAt(0);
+            mSwitch = (Switch) cl.getChildAt(1);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public SettingAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public SettingAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                        int viewType) {
+        // create a new view
+        ConstraintLayout cl = (ConstraintLayout) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.settings_element, parent, false);
+        ViewHolder vh = new ViewHolder(cl);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.mSettingNameTextView.setText(mDataset[position]);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
 }
--- app/src/main/java/com/centralesupelec/ipfs/ipfsplayer/fragments/SettingsFragment.java
+++ app/src/main/java/com/centralesupelec/ipfs/ipfsplayer/fragments/SettingsFragment.java
@@ -1,18 +1,29 @@
 package com.centralesupelec.ipfs.ipfsplayer.fragments;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.centralesupelec.ipfs.ipfsplayer.R;
+import com.centralesupelec.ipfs.ipfsplayer.SettingAdapter;
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends PreferenceFragmentCompat {
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.settings_fragment, container, false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.preferences);
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+
     }
 }
